5388d5159bfe1d3a1f9b1703a0a5465c01536ec3
Ana Yaiza Rodriguez Marrero <ayrodrig@gmail.com>
2014-10-29 17:19:14 +0100

    Added License Information

--- PAT_data_repo.py
+++ PAT_data_repo.py
@@ -1,3 +1,19 @@
+## This file is part of pattuples2010.
+## Copyright (C) 2014 Instituto de Fisica de Cantabria and CERN.
+
+## pattuples2010 is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## pattuples2010 is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with pattuples2010. If not, see <http://www.gnu.org/licenses/>.
+
 ## import skeleton process
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
